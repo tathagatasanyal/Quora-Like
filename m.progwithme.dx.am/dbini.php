be074afab651383c2c3cc01a218bab0220083ff8
PRITHWIRAJ SAMANTA <prskid1000@gmail.com>
2020-11-27 09:34:57 +0530

    Update dbini.php

--- m.progwithme.dx.am/dbini.php
+++ m.progwithme.dx.am/dbini.php
@@ -1,3 +1,3 @@
 <?php
-$mysqli=new mysqli("fdb21.awardspace.net","2734291_data","HATSoff@2018","2734291_data","3306");
+$mysqli=new mysqli("fdb21.awardspace.net","2734291_data","password","2734291_data","3306");
 ?>
